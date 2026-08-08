228c5c2cd2c42d6168a48c6ea2c492c7b500a19d
subhansh-dev <843subhanshkv42021@gmail.com>
2026-08-08 15:11:41 +0530

    Convert to standard Swift package format

--- push4.bat
+++ push4.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Users\Admin\Desktop\huewaves
 git add -A
-git commit -m "Guest mode: works without credentials + CI fix"
+git commit -m "Convert to standard Swift package format"
 git push
 del %0
